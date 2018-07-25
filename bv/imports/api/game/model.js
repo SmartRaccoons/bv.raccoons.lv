f652163125b49f286c3d52532858a89787c142a1
Valuks <termilv@gmail.com>
2018-07-25 20:12:47 +0300

    game edit completed

--- bv/imports/api/game/model.js
+++ bv/imports/api/game/model.js
@@ -12,12 +12,23 @@ Game.deny({
   update() { return true; },
   remove() { return true; },
 });
+
+export const actions_default = [
+  {ev: '', points: 1},
+  {ev: 'se', points: -1},
+  {ev: 'sa', points: 1},
+  {ev: 'ae', points: -1},
+  {ev: 'ak', points: 1},
+  {ev: 'be', points: -1},
+  {ev: 'b', points: 1},
+  {ev: 'e', points: -1},
+];
 Game.helpers({
   update_attr(attr) {
     Game.update(this._id, { $set: attr });
   },
   sets_result() {
-    return this.sets.slice(0, this.ended ? this.sets.length : this.sets.length - 1).reduce((acc, v)=>{
+    return this.sets_played().reduce((acc, v)=>{
       if (v[0] > v[1]) {
         acc[0]++;
       } else {
@@ -30,13 +41,13 @@ Game.helpers({
     return this.sets[this.sets.length - 1];
   },
   _set_is_last() {
-    var settings = this._settings();
+    let settings = this._settings();
     return this.sets.length >= settings.sets
   },
   _set_point_diff(advantage) {
-    var set = this.sets.slice(-1)[0];
-    var settings = this._settings();
-    var points_end = (this._set_is_last() ? settings.set_last_points : settings.set_points) - advantage;
+    let set = this.sets.slice(-1)[0];
+    let settings = this._settings();
+    let points_end = (this._set_is_last() ? settings.set_last_points : settings.set_points) - advantage;
     if (set[0] >= points_end || set[1] >= points_end) {
       if (!settings.advantage) {
         return set[0] > set[1] ? 0 : 1;
@@ -49,26 +60,79 @@ Game.helpers({
   },
   _set_point() { return this._set_point_diff(1); },
   _set_end() { return this._set_point_diff(0) !== false; },
-  _last_set_index() {
-    this.sets.length - 1;
-  },
   _settings(){
     return settings(Meteor.users.findOne(Meteor.userId()).settings);
+  },
+  info() {
+    let set_point = this._set_point();
+    let set_last = this._set_is_last();
+    let info = ['', ''];
+    if (set_point !== false) {
+      info[set_point] = set_last ? 'M' : 'S';
+    }
+    return info;
+  },
+  timeouts() {
+    let timeouts_history = this.sets_history[this.sets.length - 1].filter((v)=>{ return v.action === 'T'});
+    return [0, 1].map((team)=>{
+      return timeouts_history.filter((v)=>{ return v.team[0] === team}).length;
+    });
+  },
+  switches() {
+    return this.sets_history[this.sets.length - 1].filter((v)=>{ return v.action === 'SW'}).length;
+  },
+  switch_highlight() {
+    let history = this.sets_history[this.sets.length - 1];
+    let settings = this._settings();
+    let points = this.sets[this.sets.length - 1];
+    let points_total = points[0] + points[1];
+    let points_max = (this._set_is_last() ? settings.set_last_points : settings.set_points);
+    if (settings.switch === 'set') {
+      return this.sets.length > 1 && points_total === 0;
+    }
+    if (points_total === 0) {
+      return false;
+    }
+    if (settings.switch === 'sum 1/3' &&
+      !(points_total % Math.ceil(points_max / 3) === 0)) {
+      return false;
+    }
+    if (settings.switch === 'first 1/2' &&
+      !(
+        ( ( points[0] % Math.ceil(points_max / 2) === 0 ) && points[0] > points[1]) ||
+        ( ( points[1] % Math.ceil(points_max / 2) === 0 ) && points[1] > points[0])
+      ) ) {
+      return false;
+    }
+    return !(history && history.length > 0 && history[history.length-1].action === 'SW');
   },
   sets_update(params) {
     if (this.ended) {
       return;
     }
-    var attr_update = {};
-    var last_index = this.sets.length - 1;
-    var team = params.team;
-    this.sets[last_index][team]++;
-    this.sets_history[last_index].push({
-      team: [team],
-      serve: [this.serve[0], this.serve[1]],
-      point: 1,
-      // action: '',
-    });
+    let attr_update = {};
+    let last_index = this.sets.length - 1;
+    let team = params.team[0];
+    let player = params.team[1];
+    if (!(team === 0 || team === 1)) {
+      return;
+    }
+    let opponent = (team + 1) % 2;
+    if (player && !(player === 0 || player === 1)) {
+      return;
+    }
+    if (actions_default.filter((v)=> v.ev === params.action).length === 0) {
+      return;
+    }
+    if (this.sets[0][0] === 0 && this.sets[0][1] === 0) {
+      attr_update.started = new Date();
+    }
+    let point = this._history_add([0, 1].indexOf(player) > -1 ? [team, player] : [team], params.action);
+    if (point === 1) {
+      this.sets[last_index][team]++;
+    } else {
+      this.sets[last_index][opponent]++;
+    }
     if (this._set_end()) {
       if (this._set_is_last()) {
         attr_update.ended = new Date();
@@ -77,13 +141,65 @@ Game.helpers({
         this.sets_history.push([]);
       }
     }
-    if (team !== this.serve[0]) {
+    if ((point === 1 && team !== this.serve[0]) || (point === -1 && team === this.serve[0])) {
       attr_update = Object.assign(attr_update, this._serve_next());
     }
     this.update_attr(Object.assign({
       sets: this.sets,
       sets_history: this.sets_history,
     }, attr_update));
+  },
+  sets_update_timeout(params) {
+    if (this.ended) {
+      return;
+    }
+    let team = params.team;
+    this._history_add([team], 'T');
+    this.update_attr({
+      sets_history: this.sets_history,
+    });
+  },
+  sets_update_switch() {
+    if (this.ended) {
+      return;
+    }
+    let history = this.sets_history[this.sets.length - 1];
+    if (history && history.length > 0 && history[history.length - 1].action === 'SW') {
+      this.sets_history[this.sets.length - 1].pop();
+    } else {
+      this._history_add([], 'SW');
+    }
+    this.update_attr({
+      'switch': !this.switch,
+      sets_history: this.sets_history,
+    });
+  },
+  sets_played() {
+    return this.sets.slice(0, this.ended ? this.sets.length : this.sets.length - 1);
+  },
+  _history_add(team, action) {
+    let point = 0;
+    let action_attr = actions_default.filter((v)=> v.ev === action)[0];
+    if (action_attr) {
+      point = action_attr.points;
+    }
+    this.sets_history[this.sets.length - 1].push({
+      team: team,
+      serve: [this.serve[0], this.serve[1]],
+      point: point,
+      action: action,
+    });
+    return point;
+  },
+  history_last() {
+    let actions_default_ev = actions_default.map((v)=> v.ev);
+    for (let i = this.sets.length - 1; i > -1; i--) {
+      for (let j = this.sets_history[i].length - 1; j > -1; j--) {
+        if (actions_default_ev.indexOf(this.sets_history[i][j].action) > -1) {
+          return this.sets_history[i][j];
+        }
+      }
+    }
   },
   _serve_next() {
     this.serve[0] = (this.serve[0] + 1) % 2;
@@ -101,35 +217,42 @@ Game.helpers({
       serve_order: this.serve_order,
     });
   },
-  edited() { return (!(this.sets[0][0] === 0 && this.sets[0][1] === 0))},
+  edited() { return this.sets_history[0].length > 0 },
   undo() {
     if (!this.edited()) {
       return;
     }
-    var attr_update = {};
-    var last_index = this.sets.length - 1;
-    var last_history_index = this.sets_history[last_index].length - 1;
-    if (last_history_index < 0) {
+    let attr_update = {};
+    let last_index = this.sets.length - 1;
+    let last_history = this.sets_history[last_index].pop();
+    if (!last_history) {
       this.sets.pop();
-      this.sets_history.pop();
       return this.undo();
     }
-    var last_history = this.sets_history[last_index][last_history_index];
-    var team = last_history.team[0];
-    var opponent = (team + 1) % 2;
-    this.sets[last_index][team]--;
+    let team = last_history.team[0];
+    let opponent = (team + 1) % 2;
+    if (last_history.point === 1) {
+      this.sets[last_index][team]--;
+    }
+    if (last_history.point === -1) {
+      this.sets[last_index][opponent]--;
+    }
+    if (last_history.action === 'SW') {
+      attr_update.switch = !this.switch;
+    }
     if (this.serve[0] !== last_history.serve[0]) {
       this.serve_order[opponent] = (this.serve_order[opponent] + 1) % 2;
     }
     if (this.ended) {
       attr_update.ended = null;
     }
-    this.serve = [last_history.serve[0], last_history.serve[1]];
-    this.sets_history[last_index].pop();
+    if (this.sets_history[0].length === 0) {
+      attr_update.started = null;
+    }
     this.update_attr(Object.assign({
       sets: this.sets,
       sets_history: this.sets_history,
-      serve: this.serve,
+      serve: [last_history.serve[0], last_history.serve[1]],
       serve_order: this.serve_order,
     }, attr_update));
   },
@@ -146,7 +269,7 @@ permissions = {
   },
   owner: function (Collection, fn) {
     return permissions.login(function (params) {
-        var ob;
+        let ob;
         if (params && params._id) {
           check(params._id, String);
           ob = Collection.findOne(params._id);
@@ -183,6 +306,7 @@ if (Meteor.isServer) {
         teams: [null, null],
         sets: [[0, 0]],
         sets_history: [[]],
+        started: null,
         ended: null,
       });
       return id;
@@ -198,10 +322,13 @@ if (Meteor.isServer) {
 }
 Meteor.methods({
   'game.update.switch': permissions.owner(Game, function (_, ob) {
-    Game.update(ob._id, { $set: { 'switch': !ob.switch } });
+    ob.sets_update_switch();
   }),
   'game.update.point': permissions.owner(Game, function (params, ob) {
-    ob.sets_update({team: params.team});
+    ob.sets_update({team: params.team, action: params.action});
+  }),
+  'game.update.timeout': permissions.owner(Game, function (params, ob) {
+    ob.sets_update_timeout({team: params.team});
   }),
   'game.update.serve': permissions.owner(Game, function (params, ob) {
     ob.serve_update({serve: params.serve});
